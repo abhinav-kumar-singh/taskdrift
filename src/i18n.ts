f801800ba59328961d1862de93abff1a529ca756
abhinav-kumar-singh <singhankur111968@gmail.com>
2024-09-01 21:33:24 +0530

    first commit

---
+++ src/i18n.ts
@@ -0,0 +1,35 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import { StorageKey } from "./common/component-lib/storage-manager/storage.types";
+
+
+i18n
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ["en", "hi", "fr", "es", "de"], // english, hindi,french,espanol,german
+    fallbackLng: "en",
+    detection: {
+      order: [
+        "localStorage",
+        "sessionStorage",
+        "navigator",
+        "htmlTag",
+        "path",
+        "subdomain",
+      ],
+      caches: ["localStorage"],
+      lookupLocalStorage: StorageKey.SELECTED_LANGUAGE,
+    },
+    backend: {
+      loadPath: "/locales/{{lng}}/translation.json",
+    },
+    react: {
+      useSuspense: false,
+    },
+  });
+
+export default i18n;
